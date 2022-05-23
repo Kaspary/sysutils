4fcb18c092b10a29e64aba3ecb214790011bcd33
Kaspary <joaopedrokaspary@hotmail.com>
2022-05-23 14:21:15 -0300

    fix: fix sintax error

--- config/git.config.sh
+++ config/git.config.sh
@@ -5,6 +5,6 @@ read -p 'E-mail: ' email;
 read -p 'Password cache in minutes [30]: ' cache;
 cache=${cache:-30};
 
-git config --global user.name $name;
-git config --global user.email $email;
+git config --global user.name "${name}";
+git config --global user.email "${email}";
 git config --global credential.helper "cache --timeout=$((cache*60))";
