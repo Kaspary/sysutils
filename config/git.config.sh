c2679accd9e1a923901714f3ef4d132693cb3c9a
Kaspary <joaopedrokaspary@hotmail.com>
2022-05-23 14:09:13 -0300

    feat: Config global user to git

--- config/git.config.sh
+++ config/git.config.sh
@@ -1,2 +1,10 @@
-# Config password cache of git to 1 hour
-git config --global credential.helper "cache --timeout=1800";
+# Config global git acount and password cache of git to 1 hour
+
+read -p 'Name: ' name;
+read -p 'E-mail: ' email;
+read -p 'Password cache [30min]: ' cache;
+cache=${cache:-1800}
+
+git config --global user.name $name;
+git config --global user.email $email;
+git config --global credential.helper "cache --timeout=${cache}";
