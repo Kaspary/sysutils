0133702309ad45e9e9f2be001b5a3011dba4d8da
Kaspary <joaopedrokaspary@hotmail.com>
2024-10-01 10:55:01 -0300

    Update docker.installer.sh

--- install/docker.installer.sh
+++ install/docker.installer.sh
@@ -2,27 +2,30 @@
 
 # https://docs.docker.com/engine/install/ubuntu/
 
-echo 'Uninstall old versions';
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+echo 'Uninstalling old versions';
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
-echo 'Set up Docker`s apt repository';
+echo 'Setting up Docker’s apt repository';
 # Add Docker's official GPG key:
 sudo apt-get update;
-sudo apt-get install ca-certificates curl;
+sudo apt-get install -y ca-certificates curl;
 sudo install -m 0755 -d /etc/apt/keyrings;
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc;
 sudo chmod a+r /etc/apt/keyrings/docker.asc;
-# Add the repository to Apt sources:
+
+# Add the Docker repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
 sudo apt-get update;
 
-echo 'Installing latest docker version';
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y;
+echo 'Installing the latest Docker version';
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
 
-echo 'Creating docker group and set user to group';
+echo 'Creating docker group and adding user to the group';
 sudo getent group docker >/dev/null || sudo groupadd docker;
 sudo usermod -aG docker $USER;
 newgrp docker;
+
+echo 'Docker installation completed successfully';
